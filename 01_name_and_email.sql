096dc9e02e7959adf9bf7cee47b26c9fdf5be0b1
Doyun_hwang <77888901+materokatti@users.noreply.github.com>
2024-07-11 13:45:56 +0900

    Create 01_name_and_email.sql
    Select first name, last name and email from customer

---
+++ 01_name_and_email.sql
@@ -0,0 +1,12 @@
+/*
+	CREATE BY :  DevStory(materokatti@gmail.com`)
+	CREATTE DATE : 07/11/2024
+	DESCRIPTION : This query displays all customers FirstName, LastName, and Email
+*/
+
+SELECT
+	FirstName,
+	LastName,
+	Email
+FROM 
+	Customer
