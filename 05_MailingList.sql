27c4e9c89f324104581922030ccfa382185881d0
Doyun_hwang <77888901+materokatti@users.noreply.github.com>
2024-08-22 08:36:37 +0900

    Update 05_MailingList.sql : Upper and Lower function

--- 05_MailingList.sql
+++ 05_MailingList.sql
@@ -10,7 +10,10 @@ SELECT
 	Address,
 	FirstName || ' ' || LastName || ' ' || Address || '. ' || City || ' ' || State || ' ' || PostalCode AS [Mailing Address],
 	length(PostalCode),
-	substr(PostalCode, 1, 5) AS [5 Digit Posttal Code]
+	substr(PostalCode, 1, 5) AS [5 Digit Posttal Code],
+	upper(firstname) AS [First Name All caps],
+	lower(lastname) AS [Last Name All Lower]
+	
 FROM 
 	Customer
 
