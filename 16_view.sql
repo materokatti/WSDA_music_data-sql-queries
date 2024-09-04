1aa163f16aba31d66e6861b943f6eb6ac8718874
Doyun_hwang <77888901+materokatti@users.noreply.github.com>
2024-09-05 08:10:06 +0900

    Create 16_view.sql

---
+++ 16_view.sql
@@ -0,0 +1,11 @@
+ /*
+	CREATE BY :  DevStory(materokatti@gmail.com)
+	CREATTE DATE : 09/05/2024
+	Description : View
+*/
+ 
+ CREATE VIEW V_AvgTotal AS
+SELECT
+	round(avg(total),2) AS [Average Total]
+FROM 
+	Invoice
