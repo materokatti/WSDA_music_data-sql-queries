33440cdac81d9e45b8e29214d705a98224212ecb
Doyun_hwang <77888901+materokatti@users.noreply.github.com>
2024-09-06 08:57:35 +0900

    Create 18_updateData.sql

---
+++ 18_updateData.sql
@@ -0,0 +1,10 @@
+ /*
+	CREATE BY :  DevStory(materokatti@gmail.com)
+	CREATTE DATE : 09/06/2024
+	Description : Update Data
+*/
+ 
+Update Artist
+SET Name  = 'Demien Marley'
+WHERE
+	ArtistId = 276;
